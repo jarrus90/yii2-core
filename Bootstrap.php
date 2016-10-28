83a5c89453de8aee421ceefcde5917bcf41bf518
Vladimir Stovbchatij <unreality90@mail.ru>
2016-10-28 12:38:12 +0300

    Added module and bootstrap class

---
+++ Bootstrap.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace jarrus90\Core;
+
+use Yii;
+use yii\i18n\PhpMessageSource;
+use yii\base\BootstrapInterface;
+use yii\console\Application as ConsoleApplication;
+
+/**
+ * Bootstrap class registers module and user application component. It also creates some url rules which will be applied
+ * when UrlManager.enablePrettyUrl is enabled.
+ */
+class Bootstrap implements BootstrapInterface {
+
+
+    /** @inheritdoc */
+    public function bootstrap($app) {
+        /** @var Module $module */
+        /** @var \yii\db\ActiveRecord $modelName */
+        if ($app->hasModule('core') && ($module = $app->getModule('core')) instanceof Module) {
+
+            if (!isset($app->get('i18n')->translations['core*'])) {
+                $app->get('i18n')->translations['core*'] = [
+                    'class' => PhpMessageSource::className(),
+                    'basePath' => __DIR__ . '/messages',
+                    'sourceLanguage' => 'en-US'
+                ];
+            }
+        }
+    }
+
+}
---
+++ Module.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace jarrus90\Core;
+
+use yii\base\Module as BaseModule;
+
+class Module extends BaseModule {
+
+}
