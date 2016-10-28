71039ebe34b1fa52a1300c36cdaeef931dd79d31
Vladimir Stovbchatij <unreality90@mail.ru>
2016-10-28 11:24:29 +0300

    Fix translations

--- Web/layouts/backend/columnLeft.php
+++ Web/layouts/backend/columnLeft.php
@@ -2,10 +2,10 @@
     <section class="sidebar" style="height: auto;">
         <ul class="sidebar-menu">
             <?php if (ISSET(Yii::$app->extensions['jarrus90/yii2-multilang'])) { ?>
-                <li class="header"><?= Yii::t('app', 'Choose language'); ?></li>
+                <li class="header"><?= Yii::t('core', 'Choose language'); ?></li>
                 <?= \jarrus90\Multilang\Widgets\SelectWidget::widget(['layout' => '@jarrus90/Multilang/views/widgets/select/backend']); ?>
             <?php } ?>
-            <li class="header"><?= Yii::t('app', 'Main navigation'); ?></li>
+            <li class="header"><?= Yii::t('core', 'Main navigation'); ?></li>
                 <?= \jarrus90\Core\widgets\AdminMenu::widget(); ?>
         </ul>
     </section>
